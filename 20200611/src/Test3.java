15deec14650de21454c920cf23bb8db517102037
Sun-77777 <3198884722@qq.com>
2020-06-13 00:23:18 +0800

    声明

---
+++ 20200611/src/Test3.java
@@ -0,0 +1,14 @@
+public abstract class Test3 {
+    public final static native int w();
+
+    /*abstract*/ double d;
+
+    abstract /*final*/ double he();
+
+    /*default*/ String s;
+
+    
+    public static void main(String[] args) {
+
+    }
+}
