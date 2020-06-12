d08cb98b27aa47d2bb4c67ffa8fe2e943e6a578a
Sun-77777 <3198884722@qq.com>
2020-06-12 23:51:40 +0800

    笔试题

---
+++ 20200611/src/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String[] args) {
+        Object o = new Object() {
+          public boolean equals(Object obj) {
+              return true;
+          }
+        };
+        System.out.println(o.equals("Fred"));
+    }
+}
