c0770bfb4148ff31c34d1dec7e66194ca234d9f9
Sun-77777 <3198884722@qq.com>
2020-06-17 00:15:14 +0800

    练习

---
+++ 20200616/src/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                dianping();
+            }
+        };
+        t.run();
+        System.out.println("dazhong");
+    }
+    static void dianping() {
+        System.out.println("dianping");
+    }
+}
