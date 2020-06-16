75a43a95379e57f281cf9fd2c1bf73448c889388
Sun-77777 <3198884722@qq.com>
2020-06-16 23:58:59 +0800

    练习

---
+++ 20200616/src/Cat.java
@@ -0,0 +1,17 @@
+abstract class Animal {
+    abstract void say();
+}
+public class Cat extends Animal{
+    public Cat() {
+        System.out.println("nchs");
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+    }
+
+    @Override
+    void say() {
+        
+    }
+}
