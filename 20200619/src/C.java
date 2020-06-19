454cfe30da561c52cea728bcef983be1bde51da1
Sun-77777 <3198884722@qq.com>
2020-06-19 20:27:09 +0800

    练习

---
+++ 20200619/src/C.java
@@ -0,0 +1,10 @@
+public class C {
+    public static void main(String[] args) {
+        char c = '0';
+        short s = 1;
+        float f = 1f;
+        double d = 1.0;
+        double sss = c * s + f * d;
+
+    }
+}
