5fea50ade34c0e934de720cbb71dbd66b4a21ce8
Sun-77777 <3198884722@qq.com>
2020-06-25 23:58:05 +0800

    echo Server

--- 20200623/src/java16/UdpEchoServer.java
+++ 20200623/src/java16/UdpEchoServer.java
@@ -1,4 +1,30 @@
 package java16;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class UdpEchoServer {
+    private DatagramSocket socket = null;
+
+    public UdpEchoServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+    public void start() throws IOException {
+        System.out.println("服务器启动");
+        while (true) {
+            //a.接收请求
+            DatagramPacket requestPacket = new DatagramPacket(new byte[4096],4096);
+            socket.receive(requestPacket);
+            String request = new String(requestPacket.getData(),0,requestPacket.getLength()).trim();
+            //b.计算请求的响应
+            /*DatagramPacket responsePacket = process(requestPacket);
+            socket.send(responsePacket);*/
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
