bbe97a3bb6d30ea4e98bd03266ff95013715b31d
Sun-77777 <3198884722@qq.com>
2020-06-26 23:32:07 +0800

    练习

--- 20200623/src/java16/UdpEchoClient.java
+++ 20200623/src/java16/UdpEchoClient.java
@@ -1,6 +1,21 @@
 package java16;
 
-public class UdpEchoClient {
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+public class UdpEchoClient {
+    private DatagramSocket socket = null;
+    private String serverIp = null;
+    private String serverPort = null;
+
+    public UdpEchoClient(String serverIp,String serverPort) throws SocketException {
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+        socket = new DatagramSocket();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
