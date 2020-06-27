596d7f2eb5ebb9c6dfa5afed1cf0262895785aa5
Sun-77777 <3198884722@qq.com>
2020-06-27 23:21:34 +0800

    练习

---
+++ 20200623/src/Java16_0627/Http.java
@@ -0,0 +1,31 @@
+package Java16_0627;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Http {
+    private ServerSocket serverSocket = null;
+
+    public Http(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+    public void start() throws IOException {
+        System.out.println("服务器启动");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+    }
+}
