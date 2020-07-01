75df18d6ffba34de2df9b44181c9db1bf1bdfaae
Sun-77777 <3198884722@qq.com>
2020-07-01 11:23:27 +0800

    Tcpserver

--- 20200623/src/Java_0629/TcpServer.java
+++ 20200623/src/Java_0629/TcpServer.java
@@ -1,6 +1,6 @@
 package Java_0629;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,7 +13,44 @@ public class TcpServer {
 
     public void start() throws IOException {
         System.out.printf("服务器启动");
-        //先从内核中获取到一个TCP 连接
-        Socket clientSocket = serverSocket.accept();
+        
+        while (true) {
+            //先从内核中获取到一个TCP 连接
+            Socket clientSocket = serverSocket.accept();
+            
+            //处理这个连接
+            processConnection(clientSocket);
+        }
+
+    }
+
+    private void processConnection(Socket clientSocket) throws IOException {
+        System.out.printf("[%s %d] 客户端上线\n",clientSocket.getInetAddress().toString(),clientSocket.getPort());
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))){
+            while (true) {
+                //1.读取请求并解析
+                String request = bufferedReader.readLine();
+                //2.根据请求计算响应
+                String response = process(request);
+                //3.把响应返回给客户端
+                bufferedWriter.write(response + "\n");
+
+                System.out.printf("[%s:%d] req: %s resp : %s",clientSocket.getInetAddress().toString(),clientSocket.getPort());
+
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.printf("[%s : %d] 客户端下线\n",clientSocket.getInetAddress().toString(),clientSocket.getPort());
+        }
+    }
+
+    private String process(String request) {
+        return request;
+    }
+
+    public static void main(String[] args) throws IOException {
+        TcpServer server = new TcpServer(9090);
+        server.start();
     }
 }
