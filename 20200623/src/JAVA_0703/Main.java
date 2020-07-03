7c6cd15233db9f491a8efd094dcb08c70ddec6ef
Sun-77777 <3198884722@qq.com>
2020-07-03 23:50:42 +0800

    练习

--- 20200623/src/JAVA_0703/Main.java
+++ 20200623/src/JAVA_0703/Main.java
@@ -15,6 +15,26 @@ public class Main {
      private static String convertIp(String str2) {
         StringBuffer sb = new StringBuffer();
         Long n = Long.parseLong(str2);
+        String s = Long.toBinaryString(n);
+        String as = "";
+        if (s.length()<32) {
+            for (int i = 0; i < 32-s.length(); i++) {
+                as +="0";
+            }
+        }
+        s = as + s;
+        String[] ss = new String[4];
+        ss[0] = s.substring(0,8);
+        ss[1] = s.substring(8,16);
+        ss[2] = s.substring(16,24);
+        ss[3] = s.substring(24);
+        for (int i = 0; i < 4; i++) {
+            sb.append(Integer.parseInt(ss[i],2));
+            if (i != 3) {
+                sb.append(".");
+            }
+        }
+        return sb.toString();
 
     }
 
