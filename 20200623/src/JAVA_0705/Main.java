0e49554bf5ef7fa0a15e914967fe7147b991bc1e
Sun-77777 <3198884722@qq.com>
2020-07-05 20:10:07 +0800

    动态规划

--- 20200623/src/JAVA_0705/Main.java
+++ 20200623/src/JAVA_0705/Main.java
@@ -12,12 +12,22 @@ public class Main {
         dp[0] = 1;
         for (int i = 0; i < n; i++) {
             value[i] = scan.nextInt();
-            for (int j = sum; j >= 0; j--) {
+        }
+        System.out.println(bag(value,n,sum));
+    }
+
+    private static long bag(int[] value, int n, int sum) {
+        long[][] dp = new long[n+1][sum+1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
                 if (j >= value[i]) {
-                    dp[j] += dp[j-value[i]];
+                    dp[i][j] = dp[i-1][j-value[i]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        System.out.println(dp[sum]);
+        return dp[n][sum];
     }
 }
