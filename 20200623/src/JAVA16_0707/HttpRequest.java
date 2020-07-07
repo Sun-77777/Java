7f1523871d1e9b1065c99cbb7879433ddc2d52c7
Sun-77777 <3198884722@qq.com>
2020-07-07 22:25:00 +0800

    练习

---
+++ 20200623/src/JAVA16_0707/HttpRequest.java
@@ -0,0 +1,21 @@
+package JAVA16_0707;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private String method;
+    private String url;
+    private String version;
+    private Map<String,String> headers = new HashMap<>();
+    private Map<String,String> parameters = new HashMap<>();
+    private Map<String,String> cookies = new HashMap<>();
+    private String body;
+
+    public static HttpRequest buile(InputStream inputStream) {
+        HttpRequest request = new HttpRequest();
+        return request;
+    }
+    
+}
