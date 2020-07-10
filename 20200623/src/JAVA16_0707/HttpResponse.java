f143225316adcfcee17e028c4a60e2a4c664ea0c
Sun-77777 <3198884722@qq.com>
2020-07-10 23:54:06 +0800

    httpV3

--- 20200623/src/JAVA16_0707/HttpResponse.java
+++ 20200623/src/JAVA16_0707/HttpResponse.java
@@ -1,6 +1,9 @@
 package JAVA16_0707;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +19,18 @@ public class HttpResponse {
         HttpResponse response = new HttpResponse();
         response.outputStream = outputStream;
         return response;
+    }
+
+    public void flush() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+        bufferedWriter.write("version" + " " + status + " " + message);
+        headers.put("Content-Length",body.toString().getBytes().length + "");
+        for (Map.Entry<String,String> header : headers.entrySet()) {
+            bufferedWriter.write(header.getKey() + ": " + header.getValue());
+        }
+        bufferedWriter.write("\n");
+        bufferedWriter.write(body.toString());
+        bufferedWriter.flush();
     }
 
 
