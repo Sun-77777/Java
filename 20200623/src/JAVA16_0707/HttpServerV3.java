1d59283cb85abbfe7f9d8c94a71da532206f7c74
Sun-77777 <3198884722@qq.com>
2020-07-11 23:53:01 +0800

    maven

--- 20200623/src/JAVA16_0707/HttpServerV3.java
+++ 20200623/src/JAVA16_0707/HttpServerV3.java
@@ -1,7 +1,47 @@
 package JAVA16_0707;
 
+import Java16_0627.Http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HttpServerV3 {
+    private ServerSocket serverSocket = null;
+
+    public HttpServerV3(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+    public void start() throws IOException {
+        System.out.println("服务器启动");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        process(clientSocket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private void process(Socket clientSocket) throws IOException {
+        HttpRequest request = HttpRequest.build(clientSocket.getInputStream());
+        HttpResponse response = HttpResponse.build(clientSocket.getOutputStream());
+        //根据请求计算响应
+
+        response.flush();
+    }
+
     public static void main(String[] args) {
-        
+
+
     }
 }
