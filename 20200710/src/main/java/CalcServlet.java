cad13036596685c5bcc779a8a05cc2c00be3accc
Sun-77777 <3198884722@qq.com>
2020-07-12 22:31:10 +0800

    Servlet

--- 20200710/src/main/java/CalcServlet.java
+++ 20200710/src/main/java/CalcServlet.java
@@ -7,7 +7,12 @@ import java.io.IOException;
 public class CalcServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String aStr = req.getParameter("a");
+        String bStr = req.getParameter("b");
+        int a = Integer.parseInt(aStr);
+        int b = Integer.parseInt(bStr);
+        int result = a+b;
+        resp.getWriter().write(String.format("<h1>result=%d<h1>",result));
     }
 
 
