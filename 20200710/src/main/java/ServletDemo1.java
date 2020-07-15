3f3b6423bd33ab8688706a875b2c60d6cc76c9b2
Sun-77777 <3198884722@qq.com>
2020-07-15 22:50:11 +0800

    练习

--- 20200710/src/main/java/ServletDemo1.java
+++ 20200710/src/main/java/ServletDemo1.java
@@ -3,10 +3,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ServletDemo1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String firstName = req.getParameter("firstName");
+        String secondName = req.getParameter("secondName");
+        resp.setContentType("text/html; charset=utf-8");
+        Writer writer = resp.getWriter();
+        writer.write("<html>");
+        writer.write("firstName: " + firstName);
+        writer.write("<br/>");
+        writer.write("secondName: " + secondName);
+        writer.write("<br/>");
+        writer.write("</html>");
     }
 }
