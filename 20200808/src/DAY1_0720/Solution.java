d56b636bf5812758ed02b7c0a1ffd31bc60f43b9
Sun-77777 <3198884722@qq.com>
2020-07-23 23:44:09 +0800

    练习

--- 20200808/src/DAY1_0720/Solution.java
+++ 20200808/src/DAY1_0720/Solution.java
@@ -1,5 +1,7 @@
 package DAY1_0720;
-
+//把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。 输入一个非递减排序的数组的一个旋
+//转，输出旋转数组的最小元素。 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的
+//所有元素都大于0，若数组大小为0，请返回0。
 public class Solution {
     public static int minNumberInRotateArray(int [] array) {
         if (array == null && array.length == 0) {
