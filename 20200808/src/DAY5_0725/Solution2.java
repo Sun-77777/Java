8576658f4e53cacb9467a5c84fee36ab1618560c
Sun-77777 <3198884722@qq.com>
2020-07-26 23:46:21 +0800

    博客

---
+++ 20200808/src/DAY5_0725/Solution2.java
@@ -0,0 +1,37 @@
+package DAY5_0725;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution2 {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            list.add(node.val);
+        }
+        return list;
+    }
+}
