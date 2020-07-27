ab5706a93d991b774d5d4b1da35183823a84f660
Sun-77777 <3198884722@qq.com>
2020-07-27 23:33:13 +0800

    博客

--- 20200713_boke/src/main/java/model/UserDao.java
+++ 20200713_boke/src/main/java/model/UserDao.java
@@ -1,11 +1,21 @@
 package model;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class UserDao {
     void add(User user) {
         Connection connection = DBUtil.getConnection();
         String sql = "insert into user values(null,?,?)";
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
