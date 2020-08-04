49fb14bef657d8453909e1efe9eec9b548c5ae14
Sun-77777 <3198884722@qq.com>
2020-08-04 23:04:02 +0800

    线程练习

--- 20200730/src/ThreadDemo14.java
+++ 20200730/src/ThreadDemo14.java
@@ -26,21 +26,68 @@ public class ThreadDemo14 {
     }
 
     static class Worker extends Thread {
+        private PriorityBlockingQueue<Task> queue = null;
+        private Object mailBox = null;
+        public Worker(PriorityBlockingQueue<Task> queue,Object mailBox) {
+            this.queue = queue;
+            this.mailBox = mailBox;
+        }
+
         @Override
         public void run() {
-            //
+            //实现具体线程执行的内容
             while (true) {
+                //1.取出队首元素，检查时间是否到了
+                try {
+                    Task task = queue.take();
+                    long curTime = System.currentTimeMillis();
+                    if (task.time > curTime) {
+                        queue.put(task);
 
+                        synchronized (mailBox) {
+                            mailBox.wait(task.time - curTime);
+                        }
+                    } else {
+                        task.run();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
             }
         }
     }
     static class Timer {
+        private Object mailBox = new Object();
         //1.用一个类Task来描述任务
         //2.优先阻塞队列（用数据结构来组织）
         // 优先队列需要知道对象之间的大小关系，才能把优先级排列出来（才能保证队首元素是优先级最高的）
         private PriorityBlockingQueue<Task> queue = new PriorityBlockingQueue<>();
         //3.扫描线程
+        public Timer() {
+            Worker worker = new Worker(queue,mailBox);
+            worker.start();
+        }
 
+        //4.还需要提供一个方法，让调用者把任务安排进去
+        public void schedule(Runnable command,long after) {
+            Task task = new Task(command,after);
+            queue.put(task);
 
+            synchronized (mailBox) {
+                mailBox.notify();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hehe");
+            }
+        },5000);
     }
 }
