cc9d2c07dd0a31f0fc4718c801af9ed2e64fa484
Sun-77777 <3198884722@qq.com>
2020-08-06 00:09:22 +0800

    定时器

---
+++ 20200730/src/ThreadDemo15.java
@@ -0,0 +1,15 @@
+public class ThreadDemo15 {
+    static class Task {
+
+    }
+    static class Worker extends Thread {
+
+    }
+    static class Timer {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
