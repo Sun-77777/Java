133cad639578680ccb82d7162b545024798179f5
Sun-77777 <3198884722@qq.com>
2020-08-08 00:05:02 +0800

    lx

---
+++ 20200713/src/main/java/ServletDemo.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.getWriter().write("<html>");
+        resp.getWriter().write("username = " + username);
+        resp.getWriter().write("password = " + password);
+        resp.getWriter().write("</html>");
+    }
+}
