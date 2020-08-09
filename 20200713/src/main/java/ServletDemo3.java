17192e015da7d5963c346bd98000600cef728749
Sun-77777 <3198884722@qq.com>
2020-08-09 09:01:26 +0800

    lx

---
+++ 20200713/src/main/java/ServletDemo3.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //重定向一个页面
+        resp.sendRedirect("http://www.sogou.com");
+    }
+}
