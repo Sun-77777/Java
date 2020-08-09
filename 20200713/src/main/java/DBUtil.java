2e2a2752ca8911cb4ef3fd2b15015ff555a92b0b
Sun-77777 <3198884722@qq.com>
2020-08-09 09:00:54 +0800

    lx

---
+++ 20200713/src/main/java/DBUtil.java
@@ -0,0 +1,55 @@
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/order_system?";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private volatile static DataSource dataSource = null;
+
+    private static DataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+                    dataSource = new MysqlDataSource();
+                }
+            }
+        }
+        return dataSource;
+    }
+
+    private static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    private static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+}
