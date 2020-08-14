06acbf0204db798ad170f0a5337d5ce14ff59745
Sun-77777 <3198884722@qq.com>
2020-08-14 22:10:05 +0800

    练习

--- 20206688/src/service/MusicService.java
+++ 20206688/src/service/MusicService.java
@@ -45,7 +45,7 @@ public class MusicService {
         int ret = musicDao.deleteMusicById(id);
         return ret;
     }
-
+    //添加音乐到喜欢列表中
     public boolean insertLoveMusic(int userid,int musicid) {
         MusicDao musicDao = new MusicDao();
         if (!musicDao.findMusicByMusicId(userid, musicid)) {
@@ -57,4 +57,27 @@ public class MusicService {
         }
         return false;
     }
+
+    //查询用户喜欢的全部歌单
+    public List<Music> findLoveMusic(int user_id) {
+        List<Music> musicList = new ArrayList<>();
+        MusicDao musicDao = new MusicDao();
+        musicList = musicDao.findLoveMusic(user_id);
+        return musicList;
+    }
+
+    public List<Music> ifLoveMusic(String str,int user_id) {
+        List<Music> musicList = new ArrayList<>();
+        MusicDao musicDao = new MusicDao();
+        musicList = musicDao.ifMusicLove(str, user_id);
+        return musicList;
+    }
+
+    public int removeLoveMusic(int userid,int musicid) {
+        MusicDao musicDao = new MusicDao();
+        int ret = musicDao.removeLoveMusic(userid, musicid);
+        return ret;
+    }
+
+
 }
