7c8fabea03c81587ac08623b5c032d3cf01f20aa
Sun-77777 <3198884722@qq.com>
2020-08-27 14:14:18 +0800

    DBUtil

---
+++ ordersystem/src/main/java/model/DBUtil.java
@@ -0,0 +1,66 @@
+package model;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/orderSystem?characterEncoding=utf-8&useSSL=true";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    private volatile static DataSource dataSource = null;
+
+    private static DataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+                    dataSource = new MysqlDataSource();
+                    ((MysqlDataSource)dataSource).setURL(URL);
+                    ((MysqlDataSource)dataSource).setUser(USERNAME);
+                    ((MysqlDataSource)dataSource).setPassword(PASSWORD);
+                }
+            }
+        }
+        return dataSource;
+    }
+
+    private static Connection getConnection() {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("数据库连接失败！");
+        return null;
+    }
+
+    private static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
