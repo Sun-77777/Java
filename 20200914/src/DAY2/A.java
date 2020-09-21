51d1a22d065fea53d6a5a70391a997623f52c35f
Sun-77777 <3198884722@qq.com>
2020-09-21 23:11:48 +0800

    更新代码

--- 20200914/src/DAY2/A.java
+++ 20200914/src/DAY2/A.java
@@ -2,23 +2,49 @@ package DAY2;
 
 public class A {
     public static void main(String[] args) {
-        int[] arr = {2,2,3,4,3};
-        int ret = maxLength(arr);
+        String input = "15 + 2 * 3";
+        int ret = calculate(input);
         System.out.println(ret);
-        
+    }
+    public static int calculate (String input) {
+        // write code here
+        String[] s = input.split(" ");
+        String ss = String.valueOf(s);
+        char[] c = ss.toCharArray();
+        int result = 0;
+        int j = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '*') {
+                j = 1;
+                result += Integer.parseInt(String.valueOf(c[i-1]))* Integer.parseInt(String.valueOf(c[i+1]));
+            }
+            if (c[i] == '+') {
+                j = 1;
+                result += Integer.parseInt(String.valueOf(c[i-1]));
+            }
+            if (c[i] == '-') {
+                j = 1;
+                result += Integer.parseInt(String.valueOf(c[i-1]));
+                c[i+1] = (char) -c[i+1];
+            }
+            if (c[i] == '/') {
+                j = 1;
+                result += Integer.parseInt(String.valueOf(c[i-1]))/Integer.parseInt(String.valueOf(c[i+1]));
+            }
+        }
+        String ret = null;
+        if (j == 0) {
+            for (int i = 0; i < c.length; i++) {
+                ret = ret + String.valueOf(c[i]);
+            }
+            int n = Integer.parseInt(ret);
+            int d = Integer.parseInt("3");
+            System.out.println(d);
+            return n;
+        }
+        return result;
     }
 
-    public static int maxLength(int[] arr) {
-        int[] last = new int[100000];
-        int n = arr.length;
-        int res = 0;
-        int start = 0;//窗口开始位置
-        for (int i = 0; i < n; i++) {
-            int index = arr[i];
-            start = Math.max(start,last[index]);
-            res = Math.max(res,i-start+1);
-            last[index] = i+1;
-        }
-        return res;
-    }
+
+
 }
