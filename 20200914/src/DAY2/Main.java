ea942877b1aed4a0c4f2c7829e96850a59a06660
Sun-77777 <3198884722@qq.com>
2020-09-23 09:50:09 +0800

    更新代码

---
+++ 20200914/src/DAY2/Main.java
@@ -0,0 +1,14 @@
+package DAY2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        int ret = calculate(str);
+    }
+    public static int calculate(String Exception) {
+        return 0;
+    }
+}
