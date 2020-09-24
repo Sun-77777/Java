637b1826f2c27f044bec6f4449ae217de00476e3
Sun-77777 <3198884722@qq.com>
2020-09-24 21:07:12 +0800

    更新代码

---
+++ 20200914/src/DAY2/Test1.java
@@ -0,0 +1,53 @@
+package DAY2;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+    //"9 5 7 255"
+    /*public static int sort (String inData) {
+        String[] s = inData.split(" ");
+        int[] arr = new int[s.length];
+        for (int i = 0; i < s.length; i++) {
+            arr[i] = Integer.valueOf(s[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+
+            }
+        }
+
+    }
+    public static boolean isSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+
+
+
+
+
+        /*String[] str = inData.split(" ");
+        int[] arr = new int[str.length];
+        int count = 0;
+        for (int i = 0; i < str.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                count++;
+                System.out.println(count);
+                int tmp = arr[i];
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.length - 1] = tmp;
+            }
+        }
+        return count;*/
+
+}
