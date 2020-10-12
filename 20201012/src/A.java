362599483b0eeaa84d4db1eaa5e15fbce6639919
Sun-77777 <3198884722@qq.com>
2020-10-12 10:59:20 +0800

    更新代码

---
+++ 20201012/src/A.java
@@ -0,0 +1,8 @@
+public class A {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("hello");
+        sb.append("hello");
+        //单线程锁消除
+    }
+}
