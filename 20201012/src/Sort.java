724116b4ba833523c96b4259adb8b9158f612343
Sun-77777 <3198884722@qq.com>
2020-10-31 22:25:59 +0800

    练习

--- 20201012/src/Sort.java
+++ 20201012/src/Sort.java
@@ -2,7 +2,14 @@ public class Sort {
     public static void main(String[] args) {
 
     }
-    public static void insert() {
-
+    public static void insert(int[] array) {
+         for (int i = 1; i < array.length; i++) {
+             int v = array[i];
+             for (int j = i-1; j >= 0; j--) {
+                 if (array[j] > v) {
+                     array[j+1] = array[j];
+                 }
+             }
+         }
     }
 }
