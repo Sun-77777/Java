d44902ec58920b68424e94bd553d0bd17e2b23a7
Sun-77777 <3198884722@qq.com>
2021-02-02 23:58:14 +0800

    自动化脚本

---
+++ 20210201/0202.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest,time,re
+
+class Baidu(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://www.baidu.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_baidusearch(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_id("kw").click()
+        driver.find_element_by_id("kw").clear()
+        driver.find_element_by_id("kw").send_keys(u"测试")
+        driver.find_element_by_id("su").click()
+    def test_hao(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("hao123").click()
+        self.assertEqual(u"hao123_上网从这里开始",driver.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
