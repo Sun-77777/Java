b3537c9788dc63335fbd64b2efd34ca0cb4f50ce
Sun-77777 <3198884722@qq.com>
2021-02-09 15:32:17 +0800

    自动化脚本

---
+++ 20210201/0209.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:8080/java_image_server/index.html")
+driver.maximize_window()
+
+#上传  绝对路径
+#选择了图片
+driver.find_element_by_name("filename").send_keys("C:\\Users\\18591\\Pictures\\me.jpg")
+time.sleep(6)
+#点击上传按钮
+driver.find_element_by_xpath("//*[@id='blog-collapse']/form/div[2]/input").click()
+time.sleep(6)
+
+
+
