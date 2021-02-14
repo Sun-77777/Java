5a577547c94182ca771d9fa4d22c8c748744a833
Sun-77777 <3198884722@qq.com>
2021-02-14 11:47:15 +0800

    练习

---
+++ 20201012/src/Mmmmm.java
@@ -0,0 +1,11 @@
+public class Mmmmm {
+    public static void main(String[] args) {
+        Integer a = 59;
+        int b = 59;
+        Integer c = Integer.valueOf(59);
+        Integer d = new Integer(59);
+
+        System.out.println(b==a);
+        System.out.println(c.equals(d));
+    }
+}
