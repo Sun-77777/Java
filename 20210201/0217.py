f430cbac6de7a5c4fcd66bcdf68d8009b9dfd8f0
Sun-77777 <3198884722@qq.com>
2021-02-18 23:14:31 +0800

    练习

--- 20210201/0217.py
+++ 20210201/0217.py
@@ -20,4 +20,20 @@ time.sleep(5)
 #关闭弹出框
 alert.accept()
 time.sleep(5)
+
+#div块
+#点击click
+driver.find_element_by_id("show_modal").click()
+time.sleep(5)
+#定位modal-body
+div1 = driver.find_element_by_class_name("modal-body")
+driver.find_element_by_link_text("click me").click()
+time.sleep(5)
+#定位modal-footer
+div2 = driver.find_element_by_class_name("modal-footer")
+buttons = div2.find_element_by_tag_name("button")
+buttons[0].click()
+
+#定位上传文件按钮
+driver.find_element_by_tag_name("input").send_keys("d:/")
 driver.quit()
