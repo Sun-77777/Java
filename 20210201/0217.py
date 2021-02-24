2a6f75596c53ba13c3c379002d4a933a0f23b936
Sun-77777 <3198884722@qq.com>
2021-02-24 23:58:56 +0800

    练习

--- 20210201/0217.py
+++ 20210201/0217.py
@@ -8,18 +8,6 @@ time.sleep(3)
 #driver.implicitly_wait(1)
 driver.maximize_window()
 
-#定位点击按钮
-driver.find_element_by_id("tooltip").click()
-time.sleep(5)
-#得到操作alert弹出框的句柄
-alert = driver.switch_to.alert
-#输出弹出框的内容
-text = alert.text
-print("text = " + text)
-time.sleep(5)
-#关闭弹出框
-alert.accept()
-time.sleep(5)
 
 #div块
 #点击click
