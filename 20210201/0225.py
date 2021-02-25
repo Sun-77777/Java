c29f3c856d46f5ae62cdee3755f528ef20a1ed0d
Sun-77777 <3198884722@qq.com>
2021-02-25 23:31:39 +0800

    练习

---
+++ 20210201/0225.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.baidu.com/?tn=44004473_10_oem_dg")
+driver.maximize_window()
+
+driver.find_element_by_id("kw").send_keys("腾讯视频")
+driver.find_element_by_id("su").click()
+
+time.sleep(6)
+
+driver.quit()
