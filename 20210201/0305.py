1ffc5dfb5eb84cd4a5ab7f8483331df14d8a789b
Sun-77777 <3198884722@qq.com>
2021-03-05 16:39:09 +0800

    练习

---
+++ 20210201/0305.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import os
+driver = webdriver.Chrome()
+driver.get("")
+
+#浏览器的缩小
+driver.minimize_window()
+time.sleep(6)
+
+#浏览器的放大
+driver.maximize_window()
+time.sleep(6)
+
+#设置浏览器的宽和高
+driver.set_window_size(400,800)
+time.sleep(6)
+
+#浏览器的滚动条拖动顶端
+js = "var q=document.documentElement.scrollTop=0"
+driver.execute_script(js)
+time.sleep(5)
+
+#浏览器的滚动条拖动底端
+js = "var q=document.documentElement.scrollTop=100000"
+driver.execute_script(js)
+time.sleep(5)
+
+#同时控制浏览器的左右，上下
+js = "window.scrollTo(200,200);"
+driver.execute_script(js)
+
+time.sleep(8)
+driver.quit()
