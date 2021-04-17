a22f30f36541c02101fc2ca5b524ace32452518d
Sun-77777 <3198884722@qq.com>
2021-04-17 09:37:38 +0800

    测试

---
+++ ordersystem/src/test/java/model/OrderDaoTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import exception.OrderSystemException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderDaoTest {
+
+    @Test
+    public void add() throws OrderSystemException {
+
+    }
+
+    @Test
+    public void selectAll() {
+    }
+
+    @Test
+    public void selectByUserId() {
+    }
+
+    @Test
+    public void selectById() {
+    }
+
+    @Test
+    public void changeState() {
+    }
+}
