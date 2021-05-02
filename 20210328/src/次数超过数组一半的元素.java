861c928bb3226370f2d570339b11ff7178f9601e
Sun-77777 <3198884722@qq.com>
2021-05-02 23:05:40 +0800

    练习

--- 20210328/src/次数超过数组一半的元素.java
+++ 20210328/src/次数超过数组一半的元素.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 public class 次数超过数组一半的元素 {
     public static void main(String[] args) {
+        int[] array = {1,2,1,2,5,2,3,2,2,7,2};
+        System.out.println(MoreThanHalfNum_Solution(array));
 
     }
-    public int MoreThanHalfNum_Solution(int [] array) {
+    public static int MoreThanHalfNum_Solution(int [] array) {
         int n = array.length;
         if (n == 0) {
             return 0;
